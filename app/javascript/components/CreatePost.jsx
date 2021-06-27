0974d27538f9fe11e4e6ae9bd5107ffba20fed70
Kenneth Lin <kenneth@triplebyte.com>
2021-06-27 11:02:21 -0700

    add emotion styled

--- app/javascript/components/CreatePost.jsx
+++ app/javascript/components/CreatePost.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const CreatePost = () => (
-  <div>
+import { Form, TextArea } from 'semantic-ui-react'
 
-  </div>
+// TODO
+const Avatar = () => (
+  <img src="https://avatars.githubusercontent.com/u/1477555?v=4" />
+)
+
+const CreatePost = () => (
+  <Form>
+    <Avatar />
+    <TextArea placeholder="What's happening?"/>
+  </Form>
 )
 
 export default CreatePost
